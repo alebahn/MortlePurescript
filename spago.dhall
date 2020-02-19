372bcbeaaee38b9e00bd220d0620c32c6dbc74f7
Aaron Lebahn <cplusplus328@gmail.com>
2020-02-18 22:31:10 -0600

    Basic drawing to canvas working

--- spago.dhall
+++ spago.dhall
@@ -3,7 +3,8 @@ Welcome to a Spago project!
 You can edit this file as you like.
 -}
 { name = "my-project"
-, dependencies = [ "console", "effect", "psci-support" ]
+, dependencies =
+  [ "canvas", "console", "effect", "psci-support", "web-dom", "web-html" ]
 , packages = ./packages.dhall
 , sources = [ "src/**/*.purs", "test/**/*.purs" ]
 }
--- src/Main.purs
+++ src/Main.purs
@@ -2,9 +2,20 @@ module Main where
 
 import Prelude
 
+import Data.Maybe (Maybe(..))
 import Effect (Effect)
 import Effect.Class.Console (log)
+import Graphics.Canvas (getCanvasElementById, getContext2D, setFont, fillText)
 
 main :: Effect Unit
 main = do
-  log "🍝"
+  maybeCanvasElement <- getCanvasElementById "gameCanvas"
+  case maybeCanvasElement of
+    Nothing -> do
+      log "Nothing"
+    Just canvasElement -> do
+      context <- getContext2D canvasElement
+      setFont context "8px 'Press Start 2P'"
+      fillText context "MORTLE" 24.0 12.0
+      --this.canvasContext.fillText("MORTLE", 24, 12);
+      log "🍝"
