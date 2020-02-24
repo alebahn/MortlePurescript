1ac1ab34ab507b4929e5adc26ecd17cef2481278
Aaron Lebahn <cplusplus328@gmail.com>
2020-02-23 22:47:30 -0600

    Add ability to draw level

--- src/Game.purs
+++ src/Game.purs
@@ -2,12 +2,165 @@ module Game where
 
 import Prelude
 
-import Effect (Effect)
-import Graphics.Canvas (CanvasElement, Context2D, canvasToDataURL, clearRect, fillText, getCanvasDimensions, getContext2D, setFont)
+import Data.Array (mapWithIndex, (!!))
+import Data.Int (toNumber)
+import Data.Maybe (Maybe(..))
+import Effect (Effect, foreachE)
+import Effect.Class.Console (log)
+import Graphics.Canvas (CanvasElement, Context2D, canvasToDataURL, clearRect, fillRect, fillText, getCanvasDimensions, getContext2D, setFont, strokeRect)
 import Signal (Signal, runSignal, (~>), filter, merge, foldp)
 import Signal.DOM (animationFrame, keyPressed)
 
 foreign import setCanvasBackgroundURL :: CanvasElement -> String -> Effect Unit
+
+o :: Boolean
+o = false
+i :: Boolean
+i = true
+
+levels :: Array (Array (Array Boolean))
+levels = [
+  [
+    [o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
+    [o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
+    [o,o,o,o,o,o,o,o,o,o,o,o,o,i,o,o,o,o,o],
+    [o,o,o,o,o,o,o,o,o,o,o,o,o,o,i,o,o,o,o],
+    [o,o,o,i,i,i,i,i,i,i,i,i,i,i,i,i,o,o,o],
+    [o,o,o,o,o,o,o,o,o,o,o,o,o,o,i,o,o,o,o],
+    [o,o,o,o,o,o,o,o,o,o,o,o,o,i,o,o,o,o,o],
+    [o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
+    [o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
+    [o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
+    [o,o,o,o,o,o,o,o,o,i,o,o,o,o,o,o,o,o,o],
+    [o,o,o,o,o,o,o,o,o,i,o,o,o,o,o,o,o,o,o]
+  ],
+  [
+    [o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
+    [o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
+    [o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
+    [o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
+    [o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
+    [o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
+    [o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
+    [o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
+    [o,o,o,i,i,i,i,i,o,i,i,i,i,i,i,i,o,i,i],
+    [o,o,i,o,o,o,o,o,o,o,o,o,o,o,o,i,o,o,o],
+    [o,i,i,o,o,o,o,o,o,o,o,o,o,o,o,i,o,o,o],
+    [o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,i,o,o,o]
+  ],
+  [
+    [o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
+    [o,o,o,o,o,o,o,o,o,o,o,i,i,i,i,i,o,o,o],
+    [o,i,i,o,o,o,o,o,o,o,o,o,i,o,i,o,o,o,o],
+    [o,o,i,i,o,o,o,o,o,o,o,o,i,o,i,o,o,o,o],
+    [i,o,i,o,i,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
+    [o,o,i,o,o,i,o,o,o,o,o,o,o,o,o,o,o,o,o],
+    [o,i,i,o,o,o,i,o,o,o,o,o,o,o,o,o,o,o,o],
+    [o,o,i,o,o,o,o,i,o,o,o,o,o,o,o,o,o,o,o],
+    [i,o,i,o,o,o,o,o,i,o,o,o,o,o,o,o,o,o,o],
+    [o,o,i,o,o,o,o,o,o,i,o,o,o,o,o,o,o,o,o],
+    [o,i,i,o,o,o,o,o,o,o,i,o,o,o,o,o,o,o,o],
+    [o,o,i,o,o,o,o,o,o,o,o,i,o,o,o,o,o,o,o]
+  ],
+  [
+    [o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
+    [o,o,o,o,o,o,o,o,o,o,o,o,i,o,o,o,o,o,o],
+    [o,o,o,o,o,o,o,o,o,i,i,o,i,o,o,o,o,o,o],
+    [o,o,o,o,o,o,o,o,o,o,o,o,i,o,o,o,o,o,o],
+    [o,o,o,o,o,o,o,i,i,o,o,o,i,o,o,o,o,o,o],
+    [o,o,o,o,o,o,o,o,o,o,o,o,i,o,o,o,o,o,o],
+    [o,o,o,o,o,i,i,o,o,o,o,o,i,o,o,o,o,o,o],
+    [o,o,o,o,o,o,i,o,i,o,i,o,i,o,o,o,o,o,o],
+    [o,o,o,i,i,o,i,o,i,o,o,o,i,o,o,o,o,o,o],
+    [o,o,o,o,o,o,i,i,i,o,i,o,i,o,o,o,o,o,o],
+    [o,i,i,o,o,o,i,o,i,o,i,o,i,o,o,o,o,o,o],
+    [o,o,o,o,o,o,o,o,o,o,o,o,i,o,o,o,o,o,o]
+  ],
+  [
+    [o,o,o,o,o,o,o,i,o,o,o,o,o,o,o,o,o,o,o],
+    [i,i,i,o,o,o,o,i,o,o,o,o,i,i,o,i,o,o,o],
+    [o,o,o,o,o,o,o,i,o,o,o,o,i,o,o,i,o,o,o],
+    [o,o,o,i,i,i,o,o,o,o,o,o,i,o,i,i,o,o,o],
+    [o,o,o,o,o,i,o,o,o,o,o,o,i,o,o,i,o,o,o],
+    [o,i,i,o,o,i,i,i,i,i,i,o,i,i,o,i,o,o,o],
+    [o,o,o,o,o,o,o,o,o,o,i,o,i,o,o,i,o,o,o],
+    [o,o,o,o,i,i,o,o,o,o,i,o,i,o,i,i,o,o,o],
+    [o,o,o,o,i,o,o,o,o,o,i,o,o,o,o,i,o,o,o],
+    [o,o,o,o,i,o,i,i,i,o,i,i,i,i,o,i,o,o,o],
+    [o,i,o,o,o,o,o,o,o,o,o,o,o,o,o,i,o,i,i],
+    [o,i,o,o,i,i,o,o,o,o,o,o,o,o,o,i,o,o,o]
+  ],
+  [
+    [i,i,i,i,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
+    [o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
+    [o,o,o,i,o,i,i,o,o,o,o,o,o,o,o,o,o,o,o],
+    [i,i,o,i,o,o,o,o,o,i,i,o,o,o,o,o,o,o,o],
+    [o,o,o,i,o,o,o,o,o,i,o,o,o,i,i,o,o,o,o],
+    [o,i,i,i,i,i,i,o,o,i,o,o,o,o,o,o,o,o,o],
+    [o,o,o,o,o,o,o,o,o,i,o,o,i,o,o,o,o,o,o],
+    [i,i,i,o,o,o,o,o,o,i,o,o,i,o,o,o,o,o,o],
+    [o,o,o,o,o,o,o,i,o,i,o,o,i,o,o,o,o,o,o],
+    [o,o,o,i,i,i,i,o,o,i,o,o,o,o,o,i,o,i,i],
+    [i,i,o,o,o,o,o,o,o,o,o,o,o,o,o,i,o,o,o],
+    [o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,i,o,o,o]
+  ],
+  [
+    [o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
+    [o,o,o,o,o,o,o,o,i,i,i,i,i,i,i,i,i,i,o],
+    [o,o,o,o,o,o,o,i,i,o,o,o,o,o,o,o,o,o,o],
+    [o,i,i,i,i,i,i,i,i,o,o,i,i,i,i,i,i,i,i],
+    [o,o,o,o,o,o,o,o,i,o,o,i,o,o,o,o,o,o,o],
+    [i,o,o,o,o,o,o,i,i,o,o,i,o,o,o,i,o,o,o],
+    [i,i,i,i,i,i,o,o,i,o,i,i,i,i,o,i,o,o,o],
+    [o,o,o,o,o,i,i,o,i,o,o,i,o,o,o,i,o,o,o],
+    [o,o,o,o,o,i,o,o,i,o,o,i,o,i,i,i,o,o,o],
+    [o,o,i,i,o,i,o,i,i,o,o,i,o,o,o,i,o,o,o],
+    [i,o,i,o,o,o,o,o,i,i,o,i,i,i,o,i,o,o,o],
+    [o,o,i,o,o,o,i,o,i,o,o,o,o,o,o,i,o,o,o]
+  ],
+  [
+    [o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
+    [o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
+    [o,o,i,i,i,i,i,i,i,i,i,i,i,i,i,i,i,o,i],
+    [o,o,o,i,o,o,o,o,o,o,o,o,o,o,o,o,i,o,o],
+    [i,i,o,i,o,i,i,i,i,i,i,i,i,i,o,o,i,o,o],
+    [o,o,o,i,o,i,o,o,o,o,o,o,o,i,i,o,i,i,o],
+    [o,i,o,i,o,i,i,o,i,i,i,i,o,i,o,o,o,i,o],
+    [o,o,o,i,o,i,o,o,i,o,o,o,o,i,i,o,o,i,o],
+    [o,i,i,i,o,i,o,i,i,o,i,o,o,i,o,o,o,o,o],
+    [o,o,o,i,o,i,o,o,i,o,i,i,i,i,i,i,i,i,i],
+    [i,o,o,i,o,i,o,i,i,o,o,o,o,o,o,o,o,o,o],
+    [o,o,o,i,o,o,o,o,i,o,o,o,o,o,o,o,o,o,o]
+  ],
+  [
+    [i,i,o,i,o,i,i,i,i,i,o,i,o,i,i,i,i,i,o],
+    [o,o,o,i,o,o,o,o,o,o,o,i,o,o,o,o,o,o,o],
+    [o,i,i,i,i,i,o,i,o,i,i,i,i,i,o,i,o,i,i],
+    [o,o,o,i,o,o,o,i,o,o,o,i,o,o,o,i,o,o,o],
+    [i,i,o,i,o,i,i,i,i,i,o,i,o,i,i,i,i,i,o],
+    [o,o,o,i,o,o,o,i,o,o,o,i,o,o,o,i,o,o,o],
+    [o,i,i,i,i,i,o,i,o,i,i,i,i,i,o,i,o,i,i],
+    [o,o,o,i,o,o,o,i,o,o,o,i,o,o,o,i,o,o,o],
+    [i,i,o,i,o,i,i,i,i,i,o,i,o,i,i,i,i,i,o],
+    [o,o,o,i,o,o,o,i,o,o,o,i,o,o,o,i,o,o,o],
+    [o,i,i,i,i,i,o,i,o,i,i,i,i,i,o,i,o,i,i],
+    [o,o,o,o,o,o,o,i,o,o,o,o,o,o,o,i,o,o,o]
+  ],
+  [
+    [i,o,o,i,o,o,o,o,o,i,o,o,o,o,o,i,o,o,i],
+    [o,o,i,o,o,i,o,o,i,o,o,i,o,o,i,o,o,i,o],
+    [o,i,o,o,i,o,o,i,o,o,i,o,o,i,o,o,i,o,o],
+    [i,o,o,i,o,o,i,o,o,i,o,o,i,o,o,i,o,o,i],
+    [o,i,o,o,i,o,o,i,o,o,i,o,o,i,o,o,i,o,o],
+    [o,o,i,o,o,i,o,o,i,o,o,i,o,o,i,o,o,i,o],
+    [i,o,o,i,o,o,i,o,o,i,o,o,i,o,o,i,o,o,o],
+    [o,o,i,o,o,i,o,o,i,o,o,i,o,o,i,o,o,i,o],
+    [o,i,o,o,i,o,o,i,o,o,i,o,o,i,o,o,i,o,o],
+    [i,o,o,i,o,o,i,o,o,i,o,o,i,o,o,i,o,o,i],
+    [o,o,i,o,o,i,o,o,i,o,o,i,o,o,i,o,o,i,o],
+    [o,i,o,o,o,o,o,i,o,o,o,o,o,i,o,o,o,o,o]
+  ]
+]
 
 data GameState = Startup | Menu | Aim | Launch | Win
 derive instance eqGameState :: Eq GameState
@@ -23,7 +176,9 @@ type GameModel = {
   nextState :: GameState,
   menuOption :: MenuOption,
   refreshScreen :: Boolean,
-  paintScreen :: Boolean
+  paintScreen :: Boolean,
+  currentLevel :: Int,
+  nextLevel :: Int
 }
 
 data Input = KeyLeft | KeyRight | KeyUp | KeyDown | KeyOther | Frame
@@ -63,7 +218,9 @@ makeGame canvasElement context width height = {
   nextState: Menu,
   menuOption: NewGame,
   refreshScreen: false,
-  paintScreen: false
+  paintScreen: false,
+  currentLevel: -1,
+  nextLevel: -1
 }
 
 getInputFromKeycode :: Int -> Input
@@ -98,20 +255,77 @@ getInputSignal = do
   pure $ merge signalLeft $ merge signalUp $ merge signalRight $ merge signalDown $ merge signalEnter $ merge signalSpace signalFrame
 
 update :: Input -> GameModel -> GameModel
-update Frame game =
+update Frame game = --handle animation frame
   let
-    refreshScreen = game.currentState == game.nextState
+    refreshScreen = if game.currentState /= game.nextState
+      then case game.nextState of
+        Menu -> true
+        Aim -> game.currentLevel /= game.nextLevel
+        _ -> false
+      else false
   in
     let
       game' = game {
+        paintScreen = true,
         refreshScreen = refreshScreen,
-        currentState = game.nextState
+        currentState = game.nextState,
+        currentLevel = game.nextLevel
         }
     in
-      game' {
-        paintScreen = true
-      }
-update input game = game { paintScreen = false}
+      game'
+update input game = --handle keypress
+  let
+    game' = game {
+      paintScreen = false
+    }
+  in
+    case game.currentState of
+      Menu -> case input of
+        KeyUp -> game' { menuOption = NewGame }
+        KeyDown -> game' { menuOption = Continue }
+        KeyLeft -> game' -- do nothing
+        _ -> game' {nextLevel = 0, nextState = Aim}
+      _ -> game'
+
+drawCell :: GameModel -> Int -> Int -> Boolean -> Effect Unit
+drawCell game y x cell = if cell
+  then
+    let rectangle = {x: ((toNumber x) * 5.0), y: ((toNumber y) * 5.0), width: 5.0, height: 5.0}
+    in
+      fillRect game.canvasContext rectangle
+  else
+    pure unit
+
+drawRow :: GameModel -> Int -> Array Boolean -> Effect Unit
+drawRow game y row = foreachE (mapWithIndex (drawCell game y) row) identity
+
+drawPixel :: GameModel -> Number -> Number -> Effect Unit
+drawPixel game x y = do
+  let rectangle = {x: x, y: y, width: 1.0, height: 1.0}
+  fillRect game.canvasContext rectangle
+    --this.canvasContext.fillRect(93, 57, 1, 1);
+
+drawDoor :: GameModel -> Effect Unit
+drawDoor game = do
+  let rectangle = {x: 90.5, y: 55.5, width: 4.0, height: 4.0}
+  strokeRect game.canvasContext rectangle
+  drawPixel game 93.0 57.0
+
+drawLevel :: GameModel -> Effect Unit
+drawLevel game = do
+  let maybeLevelData = levels !! game.currentLevel
+  case maybeLevelData of
+    Nothing -> log ("Can't find level " <> (show game.currentLevel))
+    Just levelData -> do
+      foreachE (mapWithIndex (drawRow game) levelData) identity
+      drawDoor game
+
+level :: GameModel -> Effect Unit
+level game = do
+  clearScreen game
+  drawLevel game
+  saveToBackground game
+  clearScreen game
 
 getYCoordinateFromMenuOption :: MenuOption -> Number
 getYCoordinateFromMenuOption NewGame = 24.0
@@ -122,6 +336,7 @@ display game | game.paintScreen = do
   if game.refreshScreen
     then case game.currentState of
       Menu -> menu game
+      Aim -> level game
       _ -> pure unit
     else pure unit
   case game.currentState of
